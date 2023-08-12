f3aad0654c994b48d3c9b42535234ad532ac7678
tolking <qw13131wang@gmail.com>
2023-08-12 13:47:20 +0800

    fix: permalink

--- blog/.vitepress/theme/index.ts
+++ blog/.vitepress/theme/index.ts
@@ -1,15 +1,17 @@
 import Theme from 'vitepress-theme-ououe'
 import FlexAlias from '../components/FlexAlias.vue'
-import type { Theme as DefaultTheme } from 'vitepress'
+import { type Theme as DefaultTheme, inBrowser } from 'vitepress'
 import './index.css'
 
 const theme: DefaultTheme = {
   ...Theme,
   enhanceApp({ app, router }) {
-    const permalinkRegExp = /\/posts\/\d+\/\d+\/\d+\/([^/]*)\//
-    const math = router.route.path.match(permalinkRegExp)
-
-    math && router.go(`/posts/${math[1]}`)
+    if (inBrowser) {
+      // 重定向之前的永久链接
+      const permalinkRegExp = /\/posts\/\d+\/\d+\/\d+\/([^/]*)\//
+      const math = location.href.match(permalinkRegExp)
+      math && router.go(`/posts/${math[1]}`)
+    }
 
     app.component('FlexAlias', FlexAlias)
   },
